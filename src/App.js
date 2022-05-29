ac7f6cc19322f333a9a5fea75bcf6244a4c94fea
Jonathan Bridge <jvbridge@gmail.com>
2022-05-29 10:53:57 -0700

    added in icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navigator from "./components/Navigator";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigator />
       </header>
     </div>
   );
---
+++ src/components/Navigator.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import icon from "./assets/icon.jpg";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+function Navigator() {
+  const [navState, setNavState] = useState("about");
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt="icon"
+            src={icon}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            style={{ borderRadius: "50%" }}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigator;
