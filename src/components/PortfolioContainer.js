e22f9d764d6e29adb83da0f21f16618f4527de03
Jonathan Bridge <jvbridge@gmail.com>
2022-05-30 21:38:03 -0700

    starting the switch statment

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,11 +4,27 @@ import Navigator from "./Navigator";
 function PortfolioContainer() {
   const handlePageChange = (page) => setNavState(page);
   const [navState, setNavState] = useState("about");
+
+  const renderMain = () => {
+    switch (navState) {
+      case "about":
+        break;
+      default:
+        return (
+          <div>
+            <p>Error: hit default</p>
+          </div>
+        );
+    }
+  };
   return (
-    <Navigator
-      navState={navState}
-      handlePageChange={handlePageChange}
-    ></Navigator>
+    <>
+      <Navigator
+        navState={navState}
+        handlePageChange={handlePageChange}
+      ></Navigator>
+      {renderMain()}
+    </>
   );
 }
 
