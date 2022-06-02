b7bcaaf8b644abc1962a182bd775693174e6ce12
Jonathan Bridge <jvbridge@gmail.com>
2022-06-01 18:12:05 -0700

    added a contact header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,8 @@ import React from "react";
 import ContactForm from "./ContactForm";
 function Footer() {
   return (
-    <div className="container mb-3">
+    <div className="container my-3 pb-3">
+      <h3>Contact me!</h3>
       <ContactForm />
     </div>
   );
